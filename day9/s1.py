fd081065b80217df9ab85507a1ed9559cf8aad60
Linn <946692837@qq.com>
2020-08-04 15:56:45 +0800

    fix

---
+++ day9/s1.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+x = 10
+expr = """
+z = 30
+sum = x + y + z
+print(sum)
+"""
+def func():
+    y = 20
+    exec(expr)
+    exec(expr, {'x': 1, 'y': 2})
+    exec(expr, {'x': 1, 'y': 2}, {'y': 3, 'z': 4})
+
+func()
